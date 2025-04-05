d15ba2914518c841abdb8317b82720baf92ff5e4
Pawan Dasila <pawandasila06@gmail.com>
2025-04-05 11:31:25 +0530

    created check completeitno stuats

--- routes/job-seeker.routes.js
+++ routes/job-seeker.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createJobSeeker, getJobSeekerById, getJobSeekerByUserId } from "../controller/job-seeker.controller.js";
+import { checkCompletionStatus, createJobSeeker, getJobSeekerById, getJobSeekerByUserId } from "../controller/job-seeker.controller.js";
 import { verifyAccessToken } from "../middleware/auth.middleware.js";
 import { isJobseeker } from "../middleware/role.middleware.js";
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post('/job-seeker/create' , verifyAccessToken , isJobseeker , createJobSeeker);
 router.get('/job-seeker/me' , verifyAccessToken , isJobseeker , getJobSeekerByUserId);
+router.get('/job-seeker/check-status' , verifyAccessToken , isJobseeker , checkCompletionStatus);
 
 export default router;
